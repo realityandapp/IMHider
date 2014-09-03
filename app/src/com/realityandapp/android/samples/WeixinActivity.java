922f64739f1d6bedfc55aff5e6b768c77664e377
destinyd <destinyd_war@163.com>
2014-09-03 17:38:06 +0000

    weixin hook backpress

--- app/src/com/realityandapp/android/samples/WeixinActivity.java
+++ app/src/com/realityandapp/android/samples/WeixinActivity.java
@@ -2,16 +2,58 @@ package com.realityandapp.android.samples;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by dd on 14-9-2.
  */
 public class WeixinActivity extends Activity {
+    private int count = 0;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.weixin);
+
+        task = new TimerTask() {
+            @Override
+            public void run() {
+                Message message = new Message();
+                message.what = 1;
+                handler.sendMessage(message);
+            }
+        };
+        timer.schedule(task, 5000);
     }
+
+
+    @Override
+    public void onBackPressed() {
+        count++;
+        if(count >= 10){
+            //TODO goto weixin
+            finish();
+        }
+    }
+
+
+
+    private final Timer timer = new Timer();
+    private TimerTask task = null;
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(msg.what == 1){
+                count = 0;
+            }
+        }
+    };
+
 }
